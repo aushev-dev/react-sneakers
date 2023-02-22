141d5f741e43b70ce973b5efef31c790e8170327
Yernar <ernar_9.6@bk.ru>
2023-02-22 21:56:43 +0600

    secondchanges

--- src/App.js
+++ src/App.js
@@ -1,108 +1,32 @@
 import "./index.scss";
-
+import Card from "./components/Card";
+import Header from "./components/Header";
+import Drawer from "./components/Drawer";
 function App() {
   return (
     <div className="wrapper clear">
-      <header className="d-flex justify-between align-center p-40 ">
-        <div className="d-flex align-center">
-          <img width={40} height={40} src="/img/image_logo.svg" alt="" />
-          <div className="headerInfo">
+        <Drawer/>
+        <Header/>
+      
 
-            <h3 className="text-uppercase">React Sneakers</h3>
-            <p className="opacity-5">Магазин лучших кроссовок</p>
+      <div className="content p-40">
+        <div className="d-flex align-center justify-between mb-40">
+          <h1>All sneakers</h1>
+          <div className="search-block d-flex">
+            <img src="/img/search.svg" alt="Search" />
+            <input placeholder="Search..." />
           </div>
         </div>
-
-        <ul className="d-flex">
-          <li className="mr-30">
-            <img width={18} height={18} src="/img/cart.svg" alt="" />
-            <span>1205 руб.</span>
-          </li>
-          <li>
-            <img width={18} height={18} src="/img/user.svg" alt="" />
-          </li>
-        </ul>
-      </header>
-      <div className="content p-40">
-        <h1 className="mb-40">All sneakers</h1>
-
 
         <div className="sneakers d-flex">
-        <div className="card">
-          <img
-            width={133}
-            height={112}
-            src="/img/sneakers/1.jpg"
-            alt="Sneakers"
-          />
-          <h5>Nike Blazer Mid Suede</h5>
-          <div className="d-flex justify-between align-center">
-            <div className="d-flex flex-column ">
-              <span>Цена</span>
-              <b>12 999 руб</b>
-            </div>
-            <button className="button">
-              <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
-            </button>
-          </div>
-        </div>
-        <div className="card">
-          <img
-            width={133}
-            height={112}
-            src="/img/sneakers/2.jpg"
-            alt="Sneakers"
-          />
-          <h5>Nike Air Max 270</h5>
-          <div className="d-flex justify-between align-center">
-            <div className="d-flex flex-column ">
-              <span>Цена</span>
-              <b>12 999 руб</b>
-            </div>
-            <button className="button">
-              <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
-            </button>
-          </div>
-        </div>
-        <div className="card">
-          <img
-            width={133}
-            height={112}
-            src="/img/sneakers/3.jpg"
-            alt="Sneakers"
-          />
-          <h5>Nike Blazer Mid Suede</h5>
-          <div className="d-flex justify-between align-center">
-            <div className="d-flex flex-column ">
-              <span>Цена</span>
-              <b>12 999 руб</b>
-            </div>
-            <button className="button">
-              <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
-            </button>
-          </div>
-        </div>
-        <div className="card">
-          <img
-            width={133}
-            height={112}
-            src="/img/sneakers/4.jpg"
-            alt="Sneakers"
-          />
-          <h5>Puma X Aka Boku Future Rider</h5>
-          <div className="d-flex justify-between align-center">
-            <div className="d-flex flex-column ">
-              <span>Цена</span>
-              <b>12 999 руб</b>
-            </div>
-            <button className="button">
-              <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
-            </button>
-          </div>
-        </div>
 
+
+            <Card/>
+            <Card />
+            <Card/>
+            <Card/>
+      
         </div>
-       
       </div>
     </div>
   );

06b8ec70799f8e871055e61550c1f028fb89b554
Yernar <ernar_9.6@bk.ru>
2023-02-25 00:30:11 +0600

    thirdvers

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import "./index.scss";
 import Card from "./components/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
+
+const arr=[
+  {
+    title:'Nike Blazer Mid Suede', price:12999,imageUrl:'/img/sneakers/1.jpg'},
+    {title:'Nike Air Max 270', price:15600,imageUrl:'/img/sneakers/2.jpg'},
+    {title:'Nike Blazer Mid Suede', price:12999,imageUrl:'/img/sneakers/3.jpg'},
+    {title:'Nike Air Max 270', price:8999,imageUrl:'/img/sneakers/4.jpg'},
+]
+
 function App() {
   return (
     <div className="wrapper clear">
         <Drawer/>
         <Header/>
-      
 
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb-40">
@@ -17,15 +25,15 @@ function App() {
             <input placeholder="Search..." />
           </div>
         </div>
-
         <div className="sneakers d-flex">
+        {
+            arr.map((obj)=>{
+              return  <Card title={obj.title} 
+              price={obj.price} 
+              imageUrl={obj.imageUrl} onClick={()=>console.log(obj)}/> 
+            })
+        }
 
-
-            <Card/>
-            <Card />
-            <Card/>
-            <Card/>
-      
         </div>
       </div>
     </div>
